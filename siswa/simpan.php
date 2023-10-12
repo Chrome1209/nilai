c1558e79d94359dc72adbfefa5e11d5cfc3241e4
Chrome1209 <malisalhafidz12@gmail.com>
2023-10-12 10:23:25 +0700

    nilai

---
+++ siswa/simpan.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_POST['simpan'])){
+    include_once('koneksi.php');
+    $mata_diklat = $_POST['mata_diklat'];
+    $sks    = $_POST['sks'];
+    
+
+    $sql="INSERT INTO mata_diklat SET mata_diklat='$mata_diklat', sks='$sks' ";
+    $simpan=mysqli_query($koneksi, $sql);
+    if($simpan) {
+        header('location:index.php');
+    } else {
+        include "index.php?m=mata_diklat=";
+        echo 'script language="JavaScipt">';
+        echo 'alert("data gagal ditambahkan")';
+        echo '</script>';
+    }
+} else {
+    echo "<script>window.history.back()</script>";
+}
+?>
---
+++ siswa/tambah.php
@@ -0,0 +1,31 @@
+<section class="content">
+    <div class="row">
+        <div class="col-8">
+            <h1>Modul Siswa</h1>
+        </div>
+        <div class="col-4">
+            <a href="?m=siswa&s=tampil" class="btn btn-large btn-info float-end" style="float: end;"> kembali </a>
+        </div>
+        <div class="col">
+        <form action="?m=siswa&s=simpan" method="post">
+            <div class="mb-2">
+                <label for="">no induk santri</label>
+                <input type="number" name="santri" class="form-control" required autofocus>
+            </div>
+            <div class="mb-2">
+                <label for="">nama santri</label>
+                <input type="text" name="kapasitas" class="form-control" required>
+            </div>
+            <div class="mb-2">
+                <label for="">jenis kelamin</div></label>&nbsp;
+                <input type="radio" name="jk" value="L" checked >laki-laki &nbsp;
+                <input type="radio" name="jk" value="P">perempuan
+            </div>
+            <div class="mb-2">
+                <input type="reset"class="btn btn-warning">
+                <input type="submit" name="simpan" class="btn btn-primary">
+            </div>          
+        </form>
+        </div>
+    </div>
+</section>
